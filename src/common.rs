c58e557681b3888eb7e990469b0419c4a545de31
mikasaurus2 <>
2021-11-15 23:07:54 -0800

    Added CellType

--- src/common.rs
+++ src/common.rs
@@ -21,25 +21,40 @@ pub enum CellState {
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
+pub enum CellType {
+    Corner,
+    Edge,
+    Center
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct CellCoord {
     pub row: usize,
     pub column: usize,
     // An index associated with the row and column cell. Used for
     // accessing flat arrays.
     index: usize,
+    cell_type: CellType,
 }
 
 impl CellCoord {
     pub fn new(row: usize, column: usize) -> CellCoord {
+        let cell_type = match (row, column) {
+            (0, 0) | (0, 2) | (2, 2) | (2, 0) => CellType::Corner,
+            (0, 1) | (1, 2) | (2, 1) | (1, 0) => CellType::Edge,
+            _ => CellType::Center,
+        };
+
         CellCoord {
             row,
             column,
             index: row * 3 + column,
+            cell_type,
         }
     }
 
-    pub fn get_index(&self) -> usize {
-        self.index
+    pub fn get_cell_type(&self) -> CellType {
+        self.cell_type
     }
 }
 

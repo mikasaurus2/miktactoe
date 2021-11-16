2f91de87343b7a41ba92a5689f9698352c5d71a6
mikasaurus2 <>
2021-11-15 23:17:43 -0800

    Add and hook up forking AI player

--- src/game.rs
+++ src/game.rs
@@ -4,7 +4,7 @@
 //
 use crate::board::{Board, BoardState};
 use crate::common::Marker;
-use crate::player::{human::Human, ai_random::RandomAI, ai_basic::BasicAI};
+use crate::player::{ai_basic::BasicAI, ai_forking::ForkingAI, ai_random::RandomAI, human::Human};
 
 pub struct Game {
     // Human players
@@ -12,7 +12,7 @@ pub struct Game {
     //player2: Human,
     // Computer players
     //player1: RandomAI,
-    player2: BasicAI,
+    player2: ForkingAI,
     board: Board,
 }
 
@@ -32,7 +32,8 @@ impl Game {
 
             // Computer players
             //player1: RandomAI::new(String::from("Computron"), Marker::X),
-            player2: BasicAI::new(String::from("Hal9000"), Marker::O),
+            //player2: BasicAI::new(String::from("Hal9000"), Marker::O),
+            player2: ForkingAI::new(String::from("Forking"), Marker::O),
             board: Board::new(),
         }
     }
---
+++ src/player/ai_forking.rs
@@ -0,0 +1,68 @@
+use crate::board::Board;
+use crate::common::*;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+use std::{thread, time};
+
+pub struct ForkingAI {
+    pub name: String,
+    pub marker: Marker,
+    move_set: Vec<CellCoord>,
+}
+
+// ForkingAI will make moves with the following priority:
+//   1. make a winning move
+//   2. block an opponents winning move
+//   3. create a fork if possible
+//   3. move randomly
+impl ForkingAI {
+    pub fn new(name: String, marker: Marker) -> ForkingAI {
+        let mut move_set: Vec<CellCoord> = itertools::iproduct!(0..3, 0..3)
+            .map(|(row, column)| CellCoord::new(row, column))
+            .collect();
+
+        let mut rng = thread_rng();
+        move_set.shuffle(&mut rng);
+
+        ForkingAI {
+            name,
+            marker,
+            move_set,
+        }
+    }
+
+    pub fn get_valid_move(&mut self, board: &Board) -> CellCoord {
+        println!("{}'s turn.", self.name);
+
+        // Use a sleep here so it seems like the computer is thinking a bit.
+        thread::sleep(time::Duration::from_secs(1));
+
+        // place winning move if there is one
+        if let Some(cell_coord) = board.get_winning_move(self.marker) {
+            return cell_coord;
+        }
+
+        // block opponent's winning move if they have one
+        if let Some(cell_coord) = board.get_winning_move(Marker::opposite(self.marker)) {
+            return cell_coord;
+        }
+
+        // make a fork if possible
+        if let Some(cell_coord) = board.get_forking_move(self.marker) {
+            return cell_coord;
+        }
+
+        loop {
+            let player_move = self.move_set.pop().unwrap_or_else(|| {
+                panic!(
+                    "{} ran out of generated moves. You shouldn't need this many.",
+                    self.name
+                )
+            });
+
+            if let Move::Valid = board.validate_move(player_move) {
+                break player_move;
+            }
+        }
+    }
+}
